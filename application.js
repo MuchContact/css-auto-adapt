a2f9abe373daf55746181ccf6811137aafd13681
hjliu <hjliu@thoughtworks.com>
2015-10-08 12:07:57 +0800

    使每个商品的宽度可以自动调整（放大或者缩小），取消固定宽度，这样也可以达到居中显示的目的

--- application.js
+++ application.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
+	// $(window).resize(function(){
+	// 	var cellWidth = $(".home-product-image").outerWidth(true) + 1;
+	// 	var width = parseInt($("body").width() / cellWidth) * cellWidth;
+	// 	$(".images-div").css("width", width + "px");
+	// });
 	$(window).resize(function(){
-		var cellWidth = $(".home-product-image").outerWidth(true) + 1;
-		var width = parseInt($("body").width() / cellWidth) * cellWidth;
-		$(".images-div").css("width", width + "px");
-	});	
+		var cellWidth = 330;
+		var cols = parseInt($("body").width() / cellWidth);
+		var width = "calc("+100/cols+"% - 50px)";
+		$(".home-product-image").css("width", width);
+	});
 });
